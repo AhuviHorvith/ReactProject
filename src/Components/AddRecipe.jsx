8e8688f37e23c8e5c65ad76cbf5080c10d3da65d
AhuviHorvith <a0556704158@gmail.com>
2025-01-26 22:38:41 +0200

    שינוי קטן

--- src/Components/AddRecipe.jsx
+++ src/Components/AddRecipe.jsx
@@ -36,19 +36,19 @@ const AddRecipe = () => {
 
     const { control, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            inputName: '', 
-            inputPreparationTime: '', 
-            inputProduct: '', 
-            category: '' 
+            inputName: '',
+            inputPreparationTime: '',
+            inputProduct: '',
+            category: ''
         }
     });
-    
+
     const handleChangeImage = (event) => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
-                setInputImage(reader.result); 
+                setInputImage(reader.result);
             };
             reader.readAsDataURL(file);
         } else {
@@ -63,7 +63,7 @@ const AddRecipe = () => {
             PreparationTime: data.inputPreparationTime,
             Product: arrayProduct,
             Category: data.category,
-            Img: inputImage 
+            Img: inputImage
         }));
         setRecipeName(data.inputName);
         setAlertVisible(true);
@@ -71,111 +71,114 @@ const AddRecipe = () => {
             setAlertVisible(false);
         }, 3000);
     };
-    
+
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-            <Grid container justifyContent="center">
-                <Grid item xs={12} sm={8} md={6}>
-                    <Paper elevation={3} sx={{ padding: 4, width: '400px' }}>
-                        <Typography variant="h5" component="h2" gutterBottom>
-                            Add Recipe
-                        </Typography>
-                        {alertVisible && (
-                            <Alert variant="filled" severity="success" style={{ margin: '20px' }}>
-                                המתכון "{recipeName}" נוסף בהצלחה!
-                            </Alert>
-                        )}
-                        <Box
-                            component="form"
-                            noValidate
-                            autoComplete="off"
-                            onSubmit={handleSubmit(onSubmit)}
-                        >
-                            <Controller
-                                name="inputName"
-                                control={control}
-                                rules={{ required: "שדה זה חובה" }}
-                                render={({ field }) => (
-                                    <TextField {...field} label="שם מתכון" variant="outlined" error={!!errors.inputName} helperText={errors.inputName?.message} fullWidth margin="normal" />
-                                )}
-                            />
-                            <Controller
-                                name="inputPreparationTime"
-                                control={control}
-                                rules={{
-                                    required: "שדה זה חובה",
-                                    min: { value: 5, message: "זמן ההכנה לא יכול להיות פחות מ-5 דקות" }
-                                }}
-                                render={({ field }) => (
-                                    <TextField {...field} label="זמן הכנה" variant="outlined" error={!!errors.inputPreparationTime} helperText={errors.inputPreparationTime?.message} fullWidth margin="normal" />
-                                )}
-                            />
-                            <Controller
-                                name="inputProduct"
-                                control={control}
-                                rules={{
-                                    required: "שדה זה חובה",
-                                    validate: value => value.split(",").length >= 3 || "יש להזין לפחות 3 מוצרים"
-                                }}
-                                render={({ field }) => (
-                                    <TextField {...field} label="מוצרים" variant="outlined" error={!!errors.inputProduct} helperText={errors.inputProduct?.message} fullWidth margin="normal" />
-                                )}
-                            />
-                            <Controller
-                                name="category"
-                                control={control}
-                                rules={{ required: "שדה זה חובה" }}
-                                render={({ field }) => (
-                                    <FormControl fullWidth error={!!errors.category} margin="normal">
-                                        <InputLabel id="category-label">קטגוריה</InputLabel>
-                                        <Select
-                                            labelId="category-label"
-                                            {...field}
-                                            value={field.value || ''} // ודא שהערך לא יהיה undefined
-                                        >
-                                            <MenuItem value={"חלבי"}>חלבי</MenuItem>
-                                            <MenuItem value={"בשרי"}>בשרי</MenuItem>
-                                            <MenuItem value={"פרווה"}>פרווה</MenuItem>
-                                        </Select>
-                                        {errors.category && <span>{errors.category.message}</span>}
-                                    </FormControl>
-                                )}
-                            />
-                            <Button
-                                component="label"
-                                variant="contained"
-                                style={{ backgroundColor: '#a3663b', color: 'white', margin: '10px 0' }}
-                                startIcon={<CloudUploadIcon />}
-                                fullWidth
+        <>
+            {alertVisible && (
+                <Alert variant="filled" severity="success" style={{ margin: '20px' }}>
+                    המתכון "{recipeName}" נוסף בהצלחה!
+                </Alert>
+            )}
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    marginLeft:'10vw'
+                }}
+            >
+                <Grid container justifyContent="center">
+                    <Grid item xs={12} sm={8} md={6}>
+                        <Paper elevation={3} sx={{ padding: 4, width: '400px' }}>
+                            <Typography variant="h5" component="h2" gutterBottom>
+                                Add Recipe
+                            </Typography>
+
+                            <Box
+                                component="form"
+                                noValidate
+                                autoComplete="off"
+                                onSubmit={handleSubmit(onSubmit)}
                             >
-                                Upload image
-                                <VisuallyHiddenInput
-                                    type="file"
-                                    onChange={handleChangeImage}
-                                    multiple
+                                <Controller
+                                    name="inputName"
+                                    control={control}
+                                    rules={{ required: "שדה זה חובה" }}
+                                    render={({ field }) => (
+                                        <TextField {...field} label="שם מתכון" variant="outlined" error={!!errors.inputName} helperText={errors.inputName?.message} fullWidth margin="normal" />
+                                    )}
                                 />
-                            </Button>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                style={{ backgroundColor: '#a3663b', color: 'white', margin: '10px 0' }}
-                                fullWidth
-                            >
-                                Send
-                            </Button>
-                        </Box>
-                    </Paper>
+                                <Controller
+                                    name="inputPreparationTime"
+                                    control={control}
+                                    rules={{
+                                        required: "שדה זה חובה",
+                                        min: { value: 5, message: "זמן ההכנה לא יכול להיות פחות מ-5 דקות" }
+                                    }}
+                                    render={({ field }) => (
+                                        <TextField {...field} label="זמן הכנה" variant="outlined" error={!!errors.inputPreparationTime} helperText={errors.inputPreparationTime?.message} fullWidth margin="normal" />
+                                    )}
+                                />
+                                <Controller
+                                    name="inputProduct"
+                                    control={control}
+                                    rules={{
+                                        required: "שדה זה חובה",
+                                        validate: value => value.split(",").length >= 3 || "יש להזין לפחות 3 מוצרים"
+                                    }}
+                                    render={({ field }) => (
+                                        <TextField {...field} label="מוצרים" variant="outlined" error={!!errors.inputProduct} helperText={errors.inputProduct?.message} fullWidth margin="normal" />
+                                    )}
+                                />
+                                <Controller
+                                    name="category"
+                                    control={control}
+                                    rules={{ required: "שדה זה חובה" }}
+                                    render={({ field }) => (
+                                        <FormControl fullWidth error={!!errors.category} margin="normal">
+                                            <InputLabel id="category-label">קטגוריה</InputLabel>
+                                            <Select
+                                                labelId="category-label"
+                                                {...field}
+                                                value={field.value || ''} // ודא שהערך לא יהיה undefined
+                                            >
+                                                <MenuItem value={"חלבי"}>חלבי</MenuItem>
+                                                <MenuItem value={"בשרי"}>בשרי</MenuItem>
+                                                <MenuItem value={"פרווה"}>פרווה</MenuItem>
+                                            </Select>
+                                            {errors.category && <span>{errors.category.message}</span>}
+                                        </FormControl>
+                                    )}
+                                />
+                                <Button
+                                    component="label"
+                                    variant="contained"
+                                    style={{ backgroundColor: '#a3663b', color: 'white', margin: '10px 0' }}
+                                    startIcon={<CloudUploadIcon />}
+                                    fullWidth
+                                >
+                                    Upload image
+                                    <VisuallyHiddenInput
+                                        type="file"
+                                        onChange={handleChangeImage}
+                                        multiple
+                                    />
+                                </Button>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    style={{ backgroundColor: '#a3663b', color: 'white', margin: '10px 0' }}
+                                    fullWidth
+                                >
+                                    Send
+                                </Button>
+                            </Box>
+                        </Paper>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
+            </Box></>
     );
 };
 
